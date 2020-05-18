108204aee16054f3391e0960c705bb8e0051c020
Niek Veldhuis <veldhuis@berkeley.edu>
2020-05-18 10:20:08 -0700

    admin in chapt 3

--- utils/utils.py
+++ utils/utils.py
@@ -29,6 +29,23 @@ def format_project_list(x):
     p = [x.strip() for x in p]
     return(p)
 
+import urllib.request
+
+
+
+
+class DownloadProgressBar(tqdm):
+    def update_to(self, b=1, bsize=1, tsize=None):
+        if tsize is not None:
+            self.total = tsize
+        self.update(b * bsize - self.n)
+
+
+def download_url(url, output_path):
+    with DownloadProgressBar(unit='B', unit_scale=True,
+                             miniters=1, desc=url.split('/')[-1]) as t:
+        urllib.request.urlretrieve(url, filename=output_path, reporthook=t.update_to)
+
 def oracc_download(p, server = 'penn'):
     """Downloads ZIP with JSON files from
     ORACC servers. First parameter is a list
